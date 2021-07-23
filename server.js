9bddaa4f1091e3a609b74a836ff0d8ba3f844d2a
Allan Hall <flashfantasy@outlook.com>
2021-07-23 12:14:30 -0400

    Made schema for each channel

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 app.use(express.static(staticDir));
 app.use(express.urlencoded({extended: true}))
-const Messages = require('./chatty/data.js')
+const Main = require('./chatty/mainschema.js')
+const Gamer = require('./chatty/mainschema.js')
+const Pet = require('./chatty/mainschema.js')
+
 
 app.listen(port, () => {
   console.log('listening on port: ' + port) 
@@ -20,17 +23,9 @@ mongoose.connection.on("connected", (err, res) => {
   console.log("Mongoose is connected. You done good there son.")
 })
 
-//see all chatrooms IDs as JSON
-app.get("/rooms", (req, res) => {
-  res.sendFile(path.resolve('./rooms/chattyIndex.json'))
-})
-
-//see individual chatrooms in JSON
-app.get("/rooms/:chatrooms", (req, res) => {
-  res.sendFile(path.resolve('./rooms/' + req.params.chatrooms + '.json'));
-});
 
 
+//see data from Main Chatroom
 app.get("/rooms/main", async (req, res) => {
   let mainPosts = await Messages.find({})
   console.log(mainPosts)
@@ -38,18 +33,20 @@ app.get("/rooms/main", async (req, res) => {
 });
 
 app.post("/rooms/main", async (req, res) => {
-  const post = new Messages({
+  const post = new Main({
     when: Date.now(),
     user: req.body.user,
     message: req.body.message
   })
 
   await post.save();
+  res.redirect('/Main')
 })
 
 
+
 //setting up the catch all route 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve('./client/public/index.html'))
 });
 
